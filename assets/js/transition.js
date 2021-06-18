2249fdc4a5bd439c1464d8c57240d12dd17703a0
Andrew Downs <druzydowns@gmail.com>
2021-06-18 14:53:18 -0400

    create slide deck controls

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,14 +1,50 @@
 const swup = new Swup({
+  containers: ["#swup", "#controls"],
   plugins: [new SwupSlideTheme({ reversed: true })],
 });
 
+let path,
+  currentPath,
+  currentPathValue,
+  lastSlide,
+  navlist,
+  listLength,
+  nextPath,
+  prevPath,
+  start,
+  previous,
+  next,
+  end;
+
 function init() {
-  let path = window.location.pathname.split("/")[2];
-  let navList = document.querySelectorAll(".nav-list");
+  currentPath = window.location.pathname.split("/")[2];
+  currentPathValue = parseInt(currentPath);
+  navList = document.querySelectorAll(".nav-list");
+  first = navList.length - 1;
+  lastSlide = navList.length - 1;
+  start = document.getElementById("start");
+  previous = document.getElementById("previous");
+  next = document.getElementById("next");
+  end = document.getElementById("end");
+  if (typeof currentPath != "undefined" && currentPathValue != lastSlide) {
+    next.href = `/slides/${currentPathValue + 1}/html`;
+  } else if (currentPathValue == lastSlide) {
+    next.href = `/`;
+  } else {
+    next.href = "/slides/1/html";
+  }
+
+  if (currentPathValue == 1 || typeof currentPath == "undefined") {
+    previous.href = `/`;
+  } else {
+    previous.href = `/slides/${currentPath - 1}/html`;
+  }
+
+  end.href = `/slides/${lastSlide}/html`;
 
   navList.forEach((listItem, index) => {
     let dataPath = listItem.dataset.path.split("/")[2];
-    if (path == dataPath) {
+    if (currentPath == dataPath) {
       listItem.classList.add("glow");
     } else {
       listItem.classList.remove("glow");
